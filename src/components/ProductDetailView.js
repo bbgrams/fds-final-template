7dacbfd4b1f739b34eaa9b6949bd028254a50b91
Shim Soyoung <sovv12@gmail.com>
2018-11-29 15:20:28 +0900

    수량과 옵션 수정 시 가격이 출력되는 기능

--- src/components/ProductDetailView.js
+++ src/components/ProductDetailView.js
@@ -18,6 +18,30 @@ class ProductDetailView extends Component {
       // }
     ],
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quantity: 1,
+      // I8
+      selectedOptionId: '',
+    };
+  }
+
+  handleOptionChange(e) {
+    this.setState({
+      selectedOptionId: parseInt(e.target.value),
+
+      // 옵션을 바꾸었을 때 수량을 1으로 초기화 하고싶을 때 :  => 제어되는 컴포넌트의 장점
+      quantity: 1,
+    });
+  }
+  handleQuantityChange(e) {
+    this.setState({
+      quantity: parseInt(e.target.value),
+    });
+  }
   render() {
     const {
       id,
@@ -27,13 +51,39 @@ class ProductDetailView extends Component {
       detailImgUrls,
       options,
     } = this.props;
+    const { quantity, selectedOptionId } = this.state;
+
+    const selectedOption = options.find(o => o.id === selectedOptionId);
+    const totalPrice = selectedOption && selectedOption.price * quantity;
+
     return (
       <div>
-        <select>
+        옵션
+        <select
+          value={selectedOptionId}
+          onChange={e => this.handleOptionChange(e)}
+          required
+        >
+          {/* I8 */}
+          <option disabled value="">
+            옵션을 선택하세요
+          </option>
           {options.map(o => (
-            <option value={o.id}>{o.title}</option>
+            <option key={o.id} value={o.id}>
+              {o.title}
+            </option>
           ))}
         </select>
+        수량
+        <input
+          value={quantity}
+          type="number"
+          // I9
+          min="1"
+          max="99"
+          onChange={e => this.handleQuantityChange(e)}
+        />
+        <div>가격 : {totalPrice}</div>
         <div>{id}</div>
         <div>{title}</div>
         <div>{description}</div>
