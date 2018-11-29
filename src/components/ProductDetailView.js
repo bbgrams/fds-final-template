84760f37d7b45c41fbd2400a09ba535a78016aab
Shim Soyoung <sovv12@gmail.com>
2018-11-29 14:12:40 +0900

    상품 옵션을 서버로부터 받아서 출력

--- src/components/ProductDetailView.js
+++ src/components/ProductDetailView.js
@@ -8,11 +8,32 @@ class ProductDetailView extends Component {
     description: '',
     mainImgUrl: '',
     detailImgUrls: [],
+    options: [
+      // options의 형태는 아래처럼 생겼다.
+      // {
+      //   "id": 1,
+      //   "productId": 1,
+      //   "title": "hot",
+      //   "price": 1500
+      // }
+    ],
   };
   render() {
-    const { id, title, description, mainImgUrl, detailImgUrls } = this.props;
+    const {
+      id,
+      title,
+      description,
+      mainImgUrl,
+      detailImgUrls,
+      options,
+    } = this.props;
     return (
       <div>
+        <select>
+          {options.map(o => (
+            <option value={o.id}>{o.title}</option>
+          ))}
+        </select>
         <div>{id}</div>
         <div>{title}</div>
         <div>{description}</div>
--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -17,14 +17,28 @@ export default class ProductDetail extends Component {
       description: '',
       mainImgUrl: '',
       detailImgUrls: [],
+      options: [
+        // options의 형태는 아래처럼 생겼다.
+        // {
+        //   "id": 1,
+        //   "productId": 1,
+        //   "title": "hot",
+        //   "price": 1500
+        // }
+      ],
     };
   }
   async componentDidMount() {
     // I4.
     const { productId } = this.props;
-    const { data: product } = await api.get('/products/' + productId);
+    const { data: product } = await api.get('/products/' + productId, {
+      params: {
+        _embed: 'options',
+      },
+    });
 
     this.setState({
+      // ↓서버로부터 받은 데이터를 상태에 그대로 넣어주고 있디
       ...product,
       loading: false,
     });
