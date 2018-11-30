17d511bd8d502a33d2efada1888ec1a8402326e8
Shim Soyoung <sovv12@gmail.com>
2018-11-30 12:36:04 +0900

    key 를 바꾸어서 componentDidMount가 다시 호출되도록 수정

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import ProductList from '../containers/ProductList';
 import Layout from '../components/Layout';
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends Component {
   render() {
+    const { location } = this.props;
+    // I23
+    console.log(location.search);
+    const p = new URLSearchParams(location.search);
+    console.log(p.get('category'));
+    const category = p.get('category');
     return (
       <Layout>
         <h1>Home</h1>
-        <ProductList />
+        {/* I21 */}
+        <Link to="/">All</Link>
+        <Link to="/?category=coffee">COFFEE</Link>
+        <Link to="/?category=drink">DRINK</Link>
+        <Link to="/?category=icecream">ICE CREAM</Link>
+        <Link to="/?category=ccino">CCINO</Link>
+        {/* I24 */}
+        <ProductList key={category} category={category} />
       </Layout>
     );
   }
